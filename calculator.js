781cf68d6ba27fb59ed577c49197cad62070bb9e
Jershz <joshuavasher@comcast.net>
2025-09-10 14:31:54 -0400

    Hook up all the buttons and +-*/ functionality of calculator

--- calculator.js
+++ calculator.js
@@ -10,7 +10,9 @@ const inputOutputDisplay = document.querySelector(".calculator-input-output");
 inputOutputDisplay.textContent = 0;
 const rows = 5;
 const columns = 4;
-let firstVal, secondVal, operator;
+let switchInput = false;
+let val = [], operator, input = "";
+
 
 function initializeCalculatorButtons() {
     for(let i = 0; i < rows; i++) {
@@ -20,25 +22,52 @@ function initializeCalculatorButtons() {
         buttonsContainer.appendChild(newRow);
 
         for(let i = 0; i < columns; i++) {
-            const newBtn = document.createElement("button");
-            const btnLabel = buttonLabels.shift();
-            newBtn.style.flex = "1";
-            newBtn.textContent = btnLabel;
-            newBtn.className = newBtn.textContent;
-            newBtn.addEventListener("click", () => {
-                if(newBtn.className === "CE") {
-                    inputOutputDisplay.textContent = 0;
-                    firstVal = null;
-                    secondVal = null;
-                }
-                else if(inputOutputDisplay.textContent == 0) {
-                    inputOutputDisplay.textContent = newBtn.textContent;
-                }
-                else inputOutputDisplay.textContent += newBtn.textContent;
-            });
-            newRow.appendChild(newBtn);
+            createAndAppendNewButton(newRow);
         }
     }
+}
+
+function createAndAppendNewButton(newRow) {
+    const newBtn = document.createElement("button");
+    const btnLabel = buttonLabels.shift();
+    newBtn.style.flex = "1";
+    newBtn.textContent = btnLabel;
+    newBtn.className = newBtn.textContent;
+
+    newBtn.addEventListener("click", () => {
+        if(newBtn.className === "CE") {
+            input = "";
+            inputOutputDisplay.textContent = 0;
+            val = [];
+        }
+        else if(operationLabels.includes(newBtn.className)) {
+            operator = newBtn.className;
+            if(input != "") val.push(parseFloat(input));
+            input = "";
+            if(val.length >= 2) {
+                input = operate(parseFloat(val[val.length-2]),parseFloat(val[val.length-1]),operator);
+                inputOutputDisplay.textContent = input;
+                val = [];
+                val.push(parseFloat(input));
+                input = "";
+            }
+        }
+        else if(newBtn.className === "=") {
+            if(input != "" && operator != "") val.push(parseFloat(input))
+            if(val.length >= 2) {
+                input = operate(parseFloat(val[val.length-2]),parseFloat(val[val.length-1]),operator);
+                inputOutputDisplay.textContent = input;
+                val = [];
+                operator = "";
+            }
+        }
+        else {
+            input += newBtn.className;
+            inputOutputDisplay.textContent = input;
+        }
+    });
+
+    newRow.appendChild(newBtn);
 }
 
 function add(a,b) {
@@ -58,6 +87,7 @@ function divide(a,b) {
 }
 
 function operate(firstVal, secondVal, operator) {
+    console.log("operator is: " + operator);
     switch(operator) {
         case "+":
             return add(firstVal, secondVal);
